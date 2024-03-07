3ea92179a1d76fd6443acde928a2d98accae7667
Huda Jawed <hudajawed18@gmail.com>
2024-03-07 14:12:21 +0500

    updated podspec and package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/realm/realm-swift.git", .exact("10.20.1")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .exact("5.0.1")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .exact("5.14.3"))
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .exact("5.14.3")),
+        .package(url: "https://github.com/socketio/socket.io-client-swift", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -34,15 +35,16 @@ let package = Package(
                         condition: .when(platforms: [.iOS])),
                 .byName(name: "SwiftyJSON", condition: .when(platforms: [.iOS])),
                 .byName(name: "SDWebImage", condition: .when(platforms: [.iOS])),
-                .product(name: "RealmSwift", package: "realm-swift")
+                .product(name: "RealmSwift", package: "realm-swift"),
+                .product(name: "SocketIO", package: "socket.io-client-swift")
                 
             ],
             path: "SwiftPM-PlatformExclude/TTChatSDKWrapper",
             exclude: []),
         .binaryTarget(
             name: "TTChatSDK",
-            url: "https://mujtabaimages.s3.ap-south-1.amazonaws.com/TTChatSDK/ios/2.5.2/spm/TTChatSDK.xcframework.zip",
-            checksum: "84be0be247c3ef37adb9b9044e0d583a1cd862318c3ae327835e1fe8d5d8fb04")
+            url: "https://ttsdk-release.s3.eu-west-1.amazonaws.com/ios/2.6.1/spm/TTChatSDK.xcframework.zip",
+            checksum: "8189ae255be532869ce57ea432448bcb15047fc314bb040d90d4919c7930e634")
         
     ]
 )
--- TTChatSDK.podspec
+++ TTChatSDK.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
     s.name           = 'TTChatSDK'
-    s.version        = '2.6.0'
+    s.version        = '2.6.1'
     s.summary        = 'Corporate Chat SDK and UI for iOS Devs'
     s.homepage       = 'https://tilismtechservices.com/sdkportal/'
     s.license        = { :type => 'MIT' }
     s.author         = { 'Huda Jawed' => 'hudajawed18@gmail.com' }
-    s.source         = { :http => 'https://mujtabaimages.s3.ap-south-1.amazonaws.com/TTChatSDK/ios/2.6.0/pod/TTChatSDK.zip'}
+    s.source         = { :http => 'https://ttsdk-release.s3.eu-west-1.amazonaws.com/ios/2.6.1/pod/TTChatSDK.zip'}
 
     s.pod_target_xcconfig = {
       'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64'
@@ -19,4 +19,5 @@ Pod::Spec.new do |s|
     s.dependency "RealmSwift","~> 10.20.1"
     s.dependency "SwiftyJSON"
     s.dependency "SDWebImage"
+    s.dependency "SocketIO"
 end
